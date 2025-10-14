2ad1ba01e787977a96b5ddf0fce6bc7823561ccb
Jehangir.khan <Jehangir.khan@LPT806>
2025-10-14 22:55:38 +0500

    Add project files.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Learning.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Learning.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+
+        [Phone]
+        [DataType(DataType.PhoneNumber)]
+         
+        
+        public string Phone { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
